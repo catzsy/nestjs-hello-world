86b6e3569f68c3f1d4ef39ad342e4742bce6c93a
Catz Sy <sy.catz@gmail.com>
2023-07-28 17:52:34 +0800

    added scripts to validate services

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,10 +8,10 @@ echo "navigate into our working directory where we have all our github files"
 cd /home/ec2-user/nestjs-hello-world
 
 #add npm and node to path
-#echo "add npm and node to the path"
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+echo "add npm and node to the path"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 echo "Install app dependencies"
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
-#echo "setting nvm paths"
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+echo "setting nvm paths"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 echo "pm2 verson"
 echo pm2 -v
 echo "killing pm2 processes now"
