006240bc6d9480bce31cba38b4c7ec2b5c9c43bc
Catz Sy <sy.catz@gmail.com>
2023-07-28 17:59:55 +0800

    change application stop to be run as ec2-user

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-#Stopping existing node servers
 echo "setting nvm paths"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-echo "pm2 verson"
-echo pm2 -v
 echo "killing pm2 processes now"
 pm2 stop hello-world
